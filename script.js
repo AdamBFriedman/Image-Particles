0b9348a2f4e381c60b5b9b7b729d7e707088670b
Adam Friedman <adam@blok.party>
2020-07-23 14:30:49 -0700

    started draw pic function added particle class

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ window.addEventListener('mousemove', (event) => {
 	mouse.x = event.x + canvas.clientLeft / 2;
 	mouse.y = event.y + canvas.clientLeft / 2;
 });
+
+// Draw function
+function drawPic() {
+	let imageWidth = png.width;
+	let imageHeight = png.height;
+	const data = ctx.getImageData(0, 0, imageWidth, imageHeight);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	class Particle {
+		constructor(x, y, color, size) {
+			(this.x = x + canvas.width / 2 - png.width * 2),
+				(this.y = y + canvas.height / 2 - png.height * 2),
+				(this.color = color),
+				(this.size = 2),
+				(this.baseX = x + canvas.width / 2 - png.width * 2),
+				(this.baseY = y + canvas.height / 2 - png.height * 2),
+				(this.density = Math.random() * 10 + 2);
+		}
+	}
+}
